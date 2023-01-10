923633200195085e69e6662f56e875582b812f35
Pushyami45 <122342930+Pushyami45@users.noreply.github.com>
2023-01-10 18:43:41 +0530

    Create 5.c

---
+++ 5.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+	int n;
+	printf("ENTER THE NUMBER:-");
+	scanf("%d",&n);
+	if(n>=0)
+	{
+		printf("GIVEN NUMBER IS +VE");
+	}
+	else
+	{
+		printf("GIVEN NUMBER IS -VE");
+    }
+    
+}
